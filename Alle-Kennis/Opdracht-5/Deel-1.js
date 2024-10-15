008986a6dd5e4a52c70fba40e27aec6b45bebfc5
ryan.barry <ryanmactavich@gmail.com>
2024-10-15 19:03:35 +0200

    My answer for exercise 5 of All knowledge

---
+++ Alle-Kennis/Opdracht-5/Deel-1.js
@@ -0,0 +1,7 @@
+const neefjesDonaldDuck = ["Kwik", "Kwek", "Kwak"];
+
+for (let i = 0; i < neefjesDonaldDuck.length; i++) {
+  console.log(neefjesDonaldDuck[i]);
+}
+
+//Deel 2 had ik perongeluk al opgelost bij het maken van deel 1. Ik probeer bij arrays altijd .length te gebruiken als ik kan.
